4b97949284a508aefd38cb1c3776e0759fdab771
nathanieladiah <nathanieladiah@gmail.com>
2022-12-18 04:14:23 -0400

    Create realtor model

--- listings/models.py
+++ listings/models.py
@@ -6,7 +6,7 @@ from realtors.models import Realtor
 
 
 class Listing(models.Model):
-	realtor = models.ForeignKey('Realtor', on_delete=models.DO_NOTHING)
+	realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
 	title = models.CharField(max_length=200)
 	address = models.CharField(max_length=200)
 	city = models.CharField(max_length=100)
@@ -19,13 +19,13 @@ class Listing(models.Model):
 	garage = models.IntegerField(default=0)
 	sqft = models.IntegerField()
 	lot_size = models.DecimalField(max_digits=5, decimal_places=1)
-	photo_main = models.ImageField(upload_to="photos/%Y%m%d/")
-	photo_1= models.ImageField(upload_to="photos/%Y%m%d/", blank=True)
-	photo_2= models.ImageField(upload_to="photos/%Y%m%d/", blank=True)
-	photo_3= models.ImageField(upload_to="photos/%Y%m%d/", blank=True)
-	photo_4= models.ImageField(upload_to="photos/%Y%m%d/", blank=True)
-	photo_5= models.ImageField(upload_to="photos/%Y%m%d/", blank=True)
-	photo_6= models.ImageField(upload_to="photos/%Y%m%d/", blank=True)
+	photo_main = models.ImageField(upload_to="photos/%Y/%m/%d/")
+	photo_1= models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+	photo_2= models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+	photo_3= models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+	photo_4= models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+	photo_5= models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+	photo_6= models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
 	is_published= models.BooleanField(default=True)
 	list_date = models.DateTimeField(default=datetime.now, blank=True)
 
