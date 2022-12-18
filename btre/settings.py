f884c8488d674388444ddf808ef58931d3529094
nathanieladiah <nathanieladiah@gmail.com>
2022-12-18 02:25:40 -0400

    Create listings and realtors apps
    Added templates for the listings apps with the bootstrap theme. Linked
    the urls and views for the listings app.

--- btre/settings.py
+++ btre/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+	'listings.apps.ListingsConfig',
     'pages.apps.PagesConfig',
+    'realtors.apps.RealtorsConfig',
 ]
 
 MIDDLEWARE = [
