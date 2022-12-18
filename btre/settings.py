28d16c7e8ed2d60bb254856ae42a97f1048b44f6
nathanieladiah <nathanieladiah@gmail.com>
2022-12-18 16:17:02 -0400

    Display listings in template

--- btre/settings.py
+++ btre/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
 	'listings.apps.ListingsConfig',
     'pages.apps.PagesConfig',
     'realtors.apps.RealtorsConfig',
+
 ]
 
 MIDDLEWARE = [
