bfddc5c6e3480afbbfc2aa419edf177fec6be40c
nathanieladiah <nathanieladiah@gmail.com>
2022-12-18 19:50:20 -0400

    Add search form filtering

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,7 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 
-from listings.choices import bedroom_choices, price_choices, state_choices
-
+from .choices import bedroom_choices, price_choices, state_choices
 from .models import Listing
 
 
@@ -31,7 +30,41 @@ def listing(request, listing_id):
 
 def search(request):
 
+	queryset_list = Listing.objects.order_by('-list_date')
+
+	# Keywords
+	if 'keywords' in request.GET:
+		keywords = request.GET['keywords']
+		if keywords:
+			queryset_list = queryset_list.filter(description__icontains=keywords)
+
+	# City
+	if 'city' in request.GET:
+		city = request.GET['city']
+		if city:
+			queryset_list = queryset_list.filter(city__iexact=city)
+
+	# State
+	if 'state' in request.GET:
+		state = request.GET['state']
+		if state:
+			queryset_list = queryset_list.filter(state__iexact=state)
+
+	# Bedrooms
+	if 'bedrooms' in request.GET:
+		bedrooms = request.GET['bedrooms']
+		if bedrooms:
+			queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+	# Price
+	if 'price' in request.GET:
+		price = request.GET['price']
+		if price:
+			queryset_list = queryset_list.filter(price__lte=price)
+
+
 	context = {
+		'listings': queryset_list,
 		'bedroom_choices': bedroom_choices,
 		'price_choices': price_choices,
 		'state_choices': state_choices
