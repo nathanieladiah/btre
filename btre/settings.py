df4ab16a5546abb294dc271a8728f594a984f239
nathanieladiah <nathanieladiah@gmail.com>
2022-12-18 03:37:43 -0400

    Setup project to use postgres

--- btre/settings.py
+++ btre/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('PG_PWD'),
+        'HOST': 'localhost',
     }
 }
 
