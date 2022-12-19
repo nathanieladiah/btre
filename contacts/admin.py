d3849d8555d0d6da6c988b4c436d604cd120fc8c
nathanieladiah <nathanieladiah@gmail.com>
2022-12-19 15:03:41 -0400

    Register contact model in admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+	list_display = ['id', 'name', 'listing', 'email', 'contact_date']
+	list_display_links = ['id', 'name']
+	search_fields = ['name', 'email', 'listing']
+	list_per_page = 25
