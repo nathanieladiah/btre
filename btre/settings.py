9ac410e234abb5d55154bc17d94930ce11b5339d
nathanieladiah <nathanieladiah@gmail.com>
2022-12-19 03:57:22 -0400

    Add accounts app and urls

--- btre/settings.py
+++ btre/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
 
+    'accounts.apps.AccountsConfig',
 	'listings.apps.ListingsConfig',
     'pages.apps.PagesConfig',
     'realtors.apps.RealtorsConfig',
