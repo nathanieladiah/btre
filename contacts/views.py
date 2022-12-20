814f613a4b77cd1497ac63a93c5bb2752cee7fb9
nathanieladiah <nathanieladiah@gmail.com>
2022-12-20 05:30:51 -0400

    Stop duplicate inquiries from users

--- contacts/views.py
+++ contacts/views.py
@@ -15,6 +15,14 @@ def contact(request):
 		user_id = request.POST['user_id']
 		realtor_email = request.POST['realtor_email']
 
+		# check if user made inquiry already
+		if request.user.is_authenticated:
+			user_id = request.user.id
+			has_contacted = Contact.objects.filter(listing_id=listing_id, user_id=user_id)
+			if has_contacted:
+				messages.error(request, 'You have already made an inquiry for this listing')
+				return redirect('listing', listing_id)
+
 		contact = Contact(
 			listing=listing,
 			listing_id=listing_id,
